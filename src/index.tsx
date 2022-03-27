7646646b791cd98b66e2351f69e3335956940427
jae kaplan <me@jkap.io>
2022-03-27 10:06:36 -0400

    service workers

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import { render } from "react-dom";
 import { App } from "./App";
 import "./style.scss";
 
+navigator.serviceWorker.register(
+    new URL("service-worker.js", import.meta.url),
+    { type: "module" }
+);
+
 const rootElement = document.getElementById("root");
 render(
     <React.StrictMode>
